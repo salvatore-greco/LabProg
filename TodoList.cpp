e5748460c5e0662b058c9bb626c28216ce30cb63
salvatore-greco <greco.salvatore03@gmail.com>
2024-05-11 00:27:09 +0200

    implemented addActivity, removeActivity, completeActivity and showAllActivity

--- TodoList.cpp
+++ TodoList.cpp
@@ -1,15 +1,54 @@
-//
-// Created by salvatore on 09/05/24.
-//
-
 #include "TodoList.h"
 
-
 void TodoList::addActivity(string name, string descr, Date date, Priority p) {
-    try{
-        Activity act (name, descr, date, p);
-        todo.push_back(act);
-    } catch (std::invalid_argument& e) {
+    try {
+        Activity act(name, descr, date, p);
+        todos.push_back(act); //todo: controlla come usare emplace, forse può fare al caso nostro
+    } catch (std::invalid_argument &e) {
         std::cerr << e.what() << " aborting" << std::endl;
     }
 }
+
+bool TodoList::removeActivity(int pos) {
+    if (todos.empty() || pos > todos.size() || pos < todos.size())
+        return false;
+    auto it = todos.begin();
+    for(int i = 0; i<pos; i++)
+        it++;
+    todos.erase(it);
+    return true;
+}
+
+bool TodoList::completeActivity(int pos) {
+    if (todos.empty() || pos > todos.size() || pos < todos.size())
+        return false;
+    auto it = todos.begin();
+    for(int i = 0; i<pos; i++)
+        it++;
+    it->setIsCompleted(true);
+    return true;
+}
+
+void TodoList::showAllActivity() {
+    int i = 1;
+    Priority p;
+    string priority;
+    cout << "indice - nome - descrizione - data di scadenza - priorità" << endl;
+    for (const auto &todo: todos) {
+        p = todo.getPriority();
+        if(p == Priority::low)
+            priority = "low";
+        else if (p == Priority::medium)
+            priority = "medium";
+        else
+            priority = "high";
+
+        cout << i << " " << todo.getName() << " " << todo.getDescription() << " " << todo.getDueDate().getDd() << "/"
+             << todo.getDueDate().getMm() << "/" << todo.getDueDate().getYyyy() << " "<<priority<<endl;
+    }
+}
+
+const string &TodoList::getName() const {
+    return name;
+}
+
--- TodoList.h
+++ TodoList.h
@@ -1,26 +1,30 @@
 #ifndef LABORATORIOPROGRAMMAZIONE_TODOLIST_H
 #define LABORATORIOPROGRAMMAZIONE_TODOLIST_H
+
 #include <list>
 #include <fstream>
 #include "Activity.h"
 #include <iostream>
 
+
 class TodoList {
 public:
-    TodoList(string n): name(n){};
+    TodoList(string n) : name(n) {};
+
     void addActivity(string name, string descr, Date date, Priority p);
-    void removeActivity(Activity act);
-    void completeActivity(Activity act); //vediamo se serve o no
+    bool removeActivity(int pos);
+    bool completeActivity(int pos); //vediamo se serve o no
     void showAllActivity();
     void showExpiringActivity(int day = 2);
     void showNotCompletedActivity();
     void showPrioritySort();
-    void saveToFile(/*file pointer, it's late i don't remember how to do it*/);
-    void restoreFromFile(/*same thing as written above*/);
-
+    void saveToFile();
+    void restoreFromFile();
+    const string &getName() const;
 private:
-    std::list<Activity> todo;
+    std::list<Activity> todos;
     string name;
+    fstream file;
 };
 
 
