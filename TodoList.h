23fbc0e558014c1b7451419d6a7dd76700068f47
salvatore-greco <greco.salvatore03@gmail.com>
2024-05-12 02:30:52 +0200

    changed container from list to unordered_map

--- TodoList.h
+++ TodoList.h
@@ -1,10 +1,11 @@
 #ifndef LABORATORIOPROGRAMMAZIONE_TODOLIST_H
 #define LABORATORIOPROGRAMMAZIONE_TODOLIST_H
 
-#include <list>
+#include <unordered_map>
 #include <algorithm>
 #include <fstream>
 #include "Activity.h"
+#include "ctime"
 #include <iostream>
 
 
@@ -13,8 +14,8 @@ public:
     TodoList(string n) : name(n) {};
 
     void addActivity(string name, string descr, Date date, Priority p);
-    bool removeActivity(int pos);
-    bool completeActivity(int pos); //vediamo se serve o no
+    bool removeActivity(const string &key);
+    bool completeActivity(const string &key);
     void showAllActivity();
     void showExpiringActivity(int day = 2);
     void showNotCompletedActivity();
@@ -23,7 +24,7 @@ public:
     void restoreFromFile();
     const string &getName() const;
 private:
-    std::list<Activity> todos;
+    std::unordered_map<string, Activity> todos;
     string name;
     fstream file;
 };
