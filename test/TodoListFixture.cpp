15a9b4c8044f57afd8db65202b000a9d4513bef4
salvatore-greco <greco.salvatore03@gmail.com>
2024-05-17 19:16:17 +0200

    testing todolist

--- test/TodoListFixture.cpp
+++ test/TodoListFixture.cpp
@@ -5,23 +5,65 @@ class TodoListFixture : public ::testing::Test {
 
 protected:
     virtual void SetUp() {
-        todos.addActivity("prima", "descrizione", Date(1,2,2024), Priority::low);
-        todos.addActivity("seconda", "descrizione", Date(2,2,2024), Priority::medium);
-        todos.addActivity("terza", "descrizione", Date(3,2,2024), Priority::high);
+        todoList.addActivity("prima", "descrizione", Date(1, 2, 2024), Priority::low);
+        todoList.addActivity("seconda", "descrizione", Date(2, 2, 2024), Priority::medium);
+        todoList.addActivity("terza", "descrizione", Date(3, 2, 2024), Priority::high);
+        todoList.addActivity("expiringActivity", "descrizione molto accurata di un attività che sta per scadere", Date(15,5,2024), Priority::high);
     }
-    TodoList todos = TodoList("test");
+    TodoList todoList = TodoList("test");
 };
 
-
 TEST_F(TodoListFixture, addActivity) {
-    int size = todos.getSize();
-    EXPECT_NO_THROW(todos.addActivity("quarta", "descrizione", Date(3,2,2024), Priority::high));
-    EXPECT_EQ(todos.getSize(), size+1);
+    int size = todoList.getSize();
+    EXPECT_NO_THROW(todoList.addActivity("quarta", "descrizione", Date(3, 2, 2024), Priority::high, false));
+    EXPECT_EQ(todoList.getSize(), size + 1);
 }
 
-TEST_F(TodoListFixture, fileHandling){
-    ASSERT_NO_THROW(todos.saveToFile());
-    todos.addActivity("quinta", "descrizione", Date(1,2,2013), Priority::low);
-    ASSERT_NO_THROW(todos.saveToFile());
+TEST_F(TodoListFixture, savingToFile){
+    ASSERT_NO_THROW(todoList.saveToFile());
+    todoList.addActivity("quinta", "descrizione", Date(1, 2, 2013), Priority::low, false);
+    ASSERT_NO_THROW(todoList.saveToFile());
+    //qui poi c'è da guardare effettivamente se il file viene scritto correttamente (spoiler: sì);
+}
+
+TEST_F(TodoListFixture, restoringFromFile){
+    TodoList copied(todoList); //copy constructor
+    ASSERT_NO_THROW(todoList.saveToFile()); //popoliamo effettivamente il file
+    todoList.removeAllActivity();
+    ASSERT_NO_THROW(todoList.restoreFromFile());
+    ASSERT_EQ(todoList.getSize(), copied.getSize());
+    //non avendo accesso alla struttura dati sottostante stampo tutte le attività dopo che ho fatto save e restore
+    cout<<"originale\n";
+    todoList.showAllActivity();
+    cout<<"copia\n";
+    copied.showAllActivity();
+}
+
+TEST_F(TodoListFixture, removingActivity){
+    int startingSize = todoList.getSize();
+    EXPECT_TRUE(todoList.removeActivity("prima"));
+    ASSERT_TRUE(startingSize > todoList.getSize());
+    EXPECT_FALSE(todoList.removeActivity("attivitanonpresente"));
+    EXPECT_FALSE(todoList.removeActivity("unaltraattivitanonpresente"));
+    todoList.removeAllActivity();
+    EXPECT_TRUE(todoList.getSize() == 0);
+    EXPECT_FALSE(todoList.removeActivity("seconda"));
+}
+
+TEST_F(TodoListFixture, completingActivity){
+    ASSERT_TRUE(todoList.completeActivity("prima"));
+    ASSERT_FALSE(todoList.completeActivity("attivitànonesistente"));
+    todoList.removeAllActivity();
+    ASSERT_FALSE(todoList.completeActivity("prima"));
+}
+
+TEST_F(TodoListFixture, printingStuff){ //questi intaseranno un po' la console, li faccio una volta e poi non li eseguo più ogni volta, solo all'occorrenza
+    cout<<"------------\n";
+    todoList.showExpiringActivity();
+    cout<<"------------ attività non completate\n";
+    todoList.completeActivity("prima");
+    todoList.showNotCompletedActivity();
+    cout<<"------------\n";
+    todoList.showPrioritySort();
 
 }
